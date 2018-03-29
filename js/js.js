9618703376a602c01f7865e720ad4dde5fc6edd4
mptu <maptu.office@gmail.com>
2018-03-29 22:19:09 +0200

    praca nad audio w js

---
+++ js/js.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.play').click(function () {
+        var $this = $(this),
+            audio = $this.siblings('audio')[0];
+
+        if (audio.paused == false) {
+            audio.pause();
+            audio.currentTime = 0;
+            $this.removeClass('playing');
+
+        } else {
+            audio.play();
+            $this.addClass('playing');
+        }
+    });
+
+
+
+});
